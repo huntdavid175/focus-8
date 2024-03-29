9cb0f9e0fbae4714b96274cb44905151b625a51e
Fawaz <fawaz@Fawazs-MacBook-Air.local>
2024-03-29 14:39:23 -0400

    added struct

--- f8.c
+++ f8.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+struct MyData
 {
+    int howMany;
+    char theText[21];
+    char directoryPath[21];
+    char filename[21];
+};
+
+int main(int argc, char *argv[])
+{
+
     return 0;
 }
