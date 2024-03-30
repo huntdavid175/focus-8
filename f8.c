351f3d47f420d9b51ca1240788e7e877c1bdcf0b
Fawaz <fawaz@Fawazs-MacBook-Air.local>
2024-03-29 22:51:46 -0400

    finished task

--- f8.c
+++ f8.c
@@ -1,51 +1,89 @@
+/*
+ * FILE : f8.cpp
+ * PROJECT : SENG1000 - Focus #8
+ * PROGRAMMER : Fawaz Dogbe (8982570)
+ * FIRST VERSION : 2024-03-29
+ * DESCRIPTION :
+ * This program receives commandline arguments and writes a file
+ */
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define kSizeString 21
+#define kSizePath 43
 
 struct MyData
 {
     int howMany;
-    char theText[21];
-    char directoryPath[21];
-    char filename[21];
+    char theText[kSizeString];
+    char directoryPath[kSizeString];
+    char filename[kSizeString];
 };
 
 int main(int argc, char *argv[])
 {
-    FILE *fp = NULL;
-    if (argc != 5)
+    FILE *fp = NULL; // File pointer
+
+    if (argc != 5) // check if right number of arguments was passed
     {
         printf("Error");
-        return -1;
+        return -2;
     }
 
-    struct MyData myArgs;
-    myArgs.howMany = atoi(argv[0]);
-
-    strcpy(myArgs.theText, argv[1]);
-    strcpy(myArgs.directoryPath, argv[2]);
-    strcpy(myArgs.filename, argv[3]);
-
-    char fileFullPath[42];
-
-    strcat(myArgs.directoryPath, myArgs.filename);
-
-    fp = fopen(fileFullPath, "w");
-
-    if (fp == NULL)
+    if ((strlen(argv[2]) > (kSizeString - 1))) // Check text to print length
     {
-        printf("Error, couldn't open file");
-        return -1;
+        printf("Program ERROR, Argument text is longer than 20 characters");
+        return -2;
+    }
+    else if (strlen(argv[3]) > (kSizeString - 1)) // Check file path name length
+    {
+        printf("Program ERROR, Argument destination path is longer than 20 character");
+        return -2;
+    }
+    else if (strlen(argv[4]) > (kSizeString - 1)) // Check file name length
+    {
+        printf("Program ERROR, Argument file name is longer than 20 characters");
+        return -2;
     }
 
-    int isPrinted = fprintf(fp, myArgs.theText);
-    if (isPrinted < 0)
+    struct MyData myArgs; // Set struct with arguments passed
+    myArgs.howMany = atoi(argv[1]);
+
+    strcpy(myArgs.theText, argv[2]);
+    strcpy(myArgs.directoryPath, argv[3]);
+    strcpy(myArgs.filename, argv[4]);
+
+    char fileFullPath[kSizePath]; // Create full path by concatenating
+
+    strcpy(fileFullPath, myArgs.directoryPath);
+    strcat(fileFullPath, "/");
+    strcat(fileFullPath, myArgs.filename);
+
+    fp = fopen(fileFullPath, "w"); // Open file
+
+    if (fp == NULL) // Check if file opened
     {
-        printf("Couldn't write to file");
-        return -1;
+        printf("Program ERROR: (File counldn't open)");
+        return -2;
     }
 
-    if (fclose(fp) != 0)
+    for (int i = 0; i < myArgs.howMany; i++) // Loop to write text to file
     {
-        printf("Error, Couldn't close file");
+        int writeToFile = fprintf(fp, "%s\n", myArgs.theText); // Write text to file in loop iteration
+
+        if (writeToFile < 0) // check if text was written
+        {
+            printf("Program ERROR: (Couldn't write to file)");
+            return -2;
+        }
+    }
+
+    int closeFile = fclose(fp); // Close file
+    if (closeFile != 0)         // Check if file closed
+    {
+        printf("Program ERROR: (Couldn't close file)");
     }
 
     return 0;
